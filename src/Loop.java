31a5d2a5ab4d829adf5f3a5101d22c9c26d89489
Sudipakhrin <Sudip.tmg48@gmail.com>
2023-06-07 18:54:45 +0545

    For,while Loop

---
+++ src/Loop.java
@@ -0,0 +1,29 @@
+public class Loop {
+    public static void  main(String args[])
+    {
+      //For loop
+
+      for(int i=1;i<=10;i++)
+      {
+          System.out.print(i + " ");
+      }
+
+      //While lopp
+
+        int a = 100; //intitalize
+        while (a<=110) //Condition
+      {
+          System.out.println(a);
+          a++; //Steps
+
+      }
+
+        //for each loop
+        //Array creation
+        int num[]= {12,13,14,15};
+        for(int x:num)
+        {
+            System.out.println(x);
+        }
+    }
+}
